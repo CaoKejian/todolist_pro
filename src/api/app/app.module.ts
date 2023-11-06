2d117c47f7677d08fbd892f0c6306a20349e5450
曹珂俭 <1849201815@qq.com>
2023-11-06 16:24:39 +0800

    chore: 根路径信息

--- src/api/app/app.module.ts
+++ src/api/app/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TodolistModule } from '../todolist/todolist.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestTest'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/todolist'),
     TodolistModule,
   ],
   controllers: [AppController],
--- src/api/app/app.service.ts
+++ src/api/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '欢迎进入TodoList后台，这是专门为瑶瑶开发的后台！可以访问 http:localhost:3000/docs 来查看接口文档';
   }
 }
